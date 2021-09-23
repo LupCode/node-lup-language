4ab87d13361521833dc9b4ee638d5bdaedaf0b64
luca-vogels <luporlp.yt@gmail.com>
2021-09-23 21:48:20 +0200

    Initial commit, but not yet finished

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # node-lup-language
 Node express middleware for detecting requested language based on:
-* URI prefix e.g. /**en**/home/
-* Cookie value e.g. lang=en
-* HTTP header e.g. Accept-Language=en-US,en;q=0.5
+1. URI prefix e.g. /**en**/home/
+2. Cookie value e.g. lang=en
+3. HTTP header e.g. Accept-Language=en-US,en;q=0.5
+4. Default language code
 
-Request gets transformed such that URI does not start with language code and new attribute 'lang' gets added to the request object.
+Request gets modified such that URI does not start with language code and new attribute `lang` gets added to the request object.
---
+++ index.js
@@ -0,0 +1,102 @@
+let DEFAULT_LANGUAGE = 'en';
+let DEFAULT_LANGUAGES = ['en'];
+let DEFAULT_COOKIE_NAME = 'L';
+let DEFAULT_COOKIE_EXPIRE = 5184000;
+let DEFAULT_COOKIE_PATH = "/";
+let DEFAULT_COOKIE_DOMAIN = null;
+let DEFAULT_COOKIE_UPDATE = true;
+
+/** Name of the output attribute set for the request that tells which language is requested */
+let DEFAULT_REQUEST_LANGUAGE_ATTR = 'lang';
+
+function LanguageRouter(options={
+    languages: DEFAULT_LANGUAGES,
+    default: DEFAULT_LANGUAGE,
+    cookieName: DEFAULT_COOKIE_NAME,
+    cookieExpire: DEFAULT_COOKIE_EXPIRE,
+    cookiePath: DEFAULT_COOKIE_PATH,
+    cookieDomain: DEFAULT_COOKIE_DOMAIN,
+    cookieUpdate: DEFAULT_COOKIE_UPDATE, 
+    langAttr: DEFAULT_REQUEST_LANGUAGE_ATTR
+}){
+    const defaultLang = options.default || DEFAULT_LANGUAGE;
+    let languages = options.languages || DEFAULT_LANGUAGES;
+    if(!(languages instanceof Array)) languages = [languages];
+    const cookieName = options.cookieName || DEFAULT_COOKIE_NAME;
+    const cookieExpire = options.cookieExpire || DEFAULT_COOKIE_EXPIRE;
+    const cookiePath = options.cookiePath || DEFAULT_COOKIE_PATH;
+    const cookieDomain = options.cookieDomain || DEFAULT_COOKIE_DOMAIN;
+    const cookieUpdate = options.cookieUpdate || DEFAULT_COOKIE_UPDATE;
+    const langAttr = options.langAttr || DEFAULT_REQUEST_LANGUAGE_ATTR;
+
+    return function(req, res, next){
+
+
+
+        const url = req.url, baseUrl = req.baseUrl, path = req.path, originalUrl = req.originalUrl; // TODO REMOVE
+        console.dir({ url, baseUrl, path, originalUrl }); // TODO REMOVE
+
+
+
+        // Parse URI
+        let lang = req.url;
+        if(lang){
+            let hasSlash = lang.startsWith("/")
+            lang = hasSlash ? lang.substring(1) : lang;
+            let idx = lang.indexOf("/");
+            lang = idx >= 0 ? lang.substring(0, idx) : lang;
+            if(languages.includes(lang)){
+
+                // updating request's url and path
+                req.url = ((hasSlash && idx < 0) ? "/" : "") + req.url.substring((hasSlash ? 1 : 0) + lang.length);
+                if(req.path){
+                    hasSlash = req.path.startsWith("/");
+                    req.path = ((hasSlash && idx < 0) ? "/" : "") + req.path.substring((hasSlash ? 1 : 0) + lang.length);
+                }
+
+            } else lang = false;
+        }
+
+        // Parse cookie
+        if(!lang && cookieName && req.headers && req.headers.cookie){
+            let langs = req.headers.cookie.split(";");
+            for(let i=0; i < langs.length; i++){
+                let entry = langs[i];
+                let idx = entry.indexOf("=");
+                if(idx <= 0) continue;
+                if((idx > 0 ? entry.substring(0, idx) : entry).trim() == cookieName){
+                    lang = (idx > 0 ? entry.substring(idx+1).trim() : "");
+                    break;
+                }
+            }
+        }
+        
+        // Parse HTTP accept language
+        if(!lang && req.headers){
+            let langs = req.headers['accept-language'];
+            if(langs){
+                langs = langs.split(/,|;/g).map(function(v){ return v.trim(); }).filter(function(v){ return v.length > 0 && !v.startsWith("q="); });
+                for(let i=0; i < langs.length; i++) if(languages.includes(langs[i])){ lang = langs[i]; break; }
+            }
+        }
+
+        lang = lang || defaultLang;
+
+        // Update cookie
+        if(cookieName && cookieUpdate){
+
+            // TODO SET COOKIE
+
+        }
+
+        req[langAttr] = lang;
+        next();
+    }
+}
+
+module.exports = {
+    DEFAULT_LANGUAGE,
+    DEFAULT_LANGUAGES,
+    DEFAULT_COOKIE_NAME,
+    LanguageRouter
+};
